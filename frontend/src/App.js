5cfea3a6d74af146f0cc990d60ae2b3decfec189
Ryan <rmassey.95@gmail.com>
2025-11-07 20:47:39 -0500

    Add empty login and register pages and implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Navbar from './Navbar';
 import HomePage from './pages/HomePage/HomePage.js';
+import Login from './pages/Login/Login.js';
+import Register from './pages/Register/Register.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <HomePage />
-    </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
