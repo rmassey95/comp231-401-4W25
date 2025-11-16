0d57640b640e315cb4e2adff1f614ae78c3ea587
Ryan <rmassey.95@gmail.com>
2025-11-15 20:06:00 -0500

    Update styling for homepage and navbar

--- app/(marketing)/_components/bootstrap-navbar.jsx
+++ app/(marketing)/_components/bootstrap-navbar.jsx
@@ -5,46 +5,33 @@ import { Logo } from "@/components/logo";
 
 function BootstrapNavbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-            <div className="container-fluid">
-                <div className="navbar-brand text-light">
-                    <Logo />
-                </div>
-                <button 
-                    className="navbar-toggler" 
-                    type="button" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#navbarSupportedContent" 
-                    aria-controls="navbarSupportedContent" 
-                    aria-expanded="false" 
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
+
+
+        <nav class="bg-neutral-primary w-full border-b border-default">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+                <Logo />
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" /></svg>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mb-2 mb-lg-0 d-flex justify-content-between w-100">
-                        <li className="nav-item">
-                            <Link href="/organization/default-org" className="nav-link text-light" aria-current="page">
-                                Home
-                            </Link>
-                        </li>
-                        <div className="d-flex">
-                            <li className="nav-item">
-                                <Link href="/login" className="nav-link text-light">
-                                    Login
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link href="/register" className="nav-link text-light">
-                                    Register
-                                </Link>
-                            </li>
-                        </div>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul class="font-medium flex p-2 gap-4 bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse ">
+                        <Link href="/organization/default-org" className="nav-link text-light" aria-current="page">
+                            Home
+                        </Link>
+                        <Link href="/login" className="">
+                            Login
+                        </Link>
+                        <Link href="/register" className="">
+                            Register
+                        </Link>
                     </ul>
                 </div>
             </div>
         </nav>
     );
+
 }
 
 export default BootstrapNavbar;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,29 +6,23 @@ import "./_components/HomePage.css";
 
 function HomePage() {
     return (
-        <div id="homepage-container">
-            <div className="position-absolute top-0 start-0 h-100 w-100 d-flex justify-content-center align-items-center bg-light">
-                <div className="w-75 container p-0 m-0">
-                    <div className="row">
-                        <div className="col d-flex flex-column justify-content-center align-items-center">
-                            <h1>Welcome to <span className="text-primary">MyTracker</span></h1>
-                            <p className="fs-5 text-center">Simple task management tool, allowing convenient organization of tasks.</p>
-                            <Link href="/login" className="btn btn-primary btn-lg width-fit" role="button">Get Organizing</Link>
-                        </div>
-                        <div className="col d-flex flex-column justify-content-center align-items-center">
-                            <div className="image-height width-fit position-relative">
-                                <Image 
-                                    src="/images/plan-5659443_1280.png" 
-                                    alt="To do task list"
-                                    width={800}
-                                    height={400}
-                                    className="img-fluid"
-                                />
-                            </div>
-                            <span className="mt-2 text-center">Image by <a href="https://pixabay.com/users/htchnm-14967706/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5659443">Htc Erl</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5659443">Pixabay</a></span>
-                        </div>
-                    </div>
+        <div className="homepage-container">
+            <div className="text-center m-10">
+                <h1>Welcome to <span className="primary-text">MyTracker</span></h1>
+                <p className="mb-4">Simple task management tool, allowing convenient organization of tasks.</p>
+                <Link href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" role="button">Get Organizing</Link>
+            </div>
+            <div className="">
+                <div className="">
+                    <Image
+                        src="/images/plan-5659443_1280.png"
+                        alt="To do task list"
+                        width={400}
+                        height={200}
+                        className=""
+                    />
                 </div>
+                <span className="mt-2 text-center">Image by <a href="https://pixabay.com/users/htchnm-14967706/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5659443" className="font-medium text-fg-brand hover:underline text-blue-500">Htc Erl</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5659443" className="font-medium text-fg-brand hover:underline text-blue-500">Pixabay</a></span>
             </div>
         </div>
     );
